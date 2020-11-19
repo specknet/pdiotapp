653bd12ec7a90d5258eb8a41f4419042f26482c1
bmogyorodi <68127470+bmogyorodi@users.noreply.github.com>
2020-11-19 15:23:35 +0000

    adding basic service function (testing how to use it)

--- app/src/main/java/com/specknet/pdiot/TrackService.java
+++ app/src/main/java/com/specknet/pdiot/TrackService.java
@@ -3,26 +3,60 @@ package com.specknet.pdiot;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
 
 import androidx.core.app.NotificationCompat;
+
+import com.specknet.pdiot.utils.Constants;
 
 import static com.specknet.pdiot.AppNotify.CHANNEL_ID;
 
 public class TrackService extends Service {
+    private int counter=0;
+    private Looper looper;
+    private MovementQueue movementQueue;
+    private BroadcastReceiver respeckLiveReceiver;
+    private IntentFilter filterTest=new IntentFilter(Constants.ACTION_INNER_RESPECK_BROADCAST);
     public TrackService() {
     }
 
     @Override
     public void onCreate() {
-        super.onCreate();
+        movementQueue=new MovementQueue(30);
+           respeckLiveReceiver=new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                if(intent.getAction() == Constants.ACTION_INNER_RESPECK_BROADCAST)
+                {
+                    float x = intent.getFloatExtra(Constants.EXTRA_RESPECK_LIVE_X, 0f);
+                    float y = intent.getFloatExtra(Constants.EXTRA_RESPECK_LIVE_Y, 0f);
+                    float z = intent.getFloatExtra(Constants.EXTRA_RESPECK_LIVE_Z, 0f);
+                    MovePoint nextPoint=new MovePoint(x,y,z);
+                    movementQueue.AddMove(nextPoint);
+
+                }
+
+            }
+        };
+        HandlerThread handlerThread=new HandlerThread("bgThread");
+        handlerThread.start();
+        looper= handlerThread.getLooper();
+        Handler handler=new Handler(looper);
+        this.registerReceiver(respeckLiveReceiver,filterTest , null,handler);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String input=intent.getStringExtra("inputExtra");
-
+        counter=1+intent.getIntExtra("counter",0);
         Intent notificationIntent= new Intent(this,MainActivity.class);
         PendingIntent pendingIntent=PendingIntent.getActivity(this,0,notificationIntent,0);
         Notification notification=new NotificationCompat.Builder(this,CHANNEL_ID)
@@ -37,7 +71,10 @@ public class TrackService extends Service {
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
+        Intent restart=new Intent(this,TrackService.class);
+        restart.putExtra("counter",counter);
+        restart.putExtra("inputExtra","Welcome "+Integer.toString(counter)+" times");
+        startService(restart);
     }
 
     @Override
