b4f401ece98d07ce02d821f1842ab08dd22f3f0e
bmogyorodi <68127470+bmogyorodi@users.noreply.github.com>
2020-11-20 00:19:17 +0000

    Testing baseline classifier and real time classification

--- app/src/main/java/com/specknet/pdiot/MovementQueue.java
+++ app/src/main/java/com/specknet/pdiot/MovementQueue.java
@@ -3,6 +3,8 @@ package com.specknet.pdiot;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.AbstractQueue;
 import java.util.Collection;
 import java.util.Iterator;
@@ -12,7 +14,7 @@ import java.util.Queue;
 
 public class MovementQueue {
 
-    private Queue<MovePoint> moves;
+    private List<MovePoint> moves;
     private int windowsize;
     public MovementQueue(int windowsize)
     {
@@ -23,8 +25,30 @@ public class MovementQueue {
     {
         moves.add(move);
         if(moves.size()>windowsize)
-            moves.remove();
+            moves.remove(0);
 
+    }
+    public ByteBuffer ConvertDataToBuffer()
+    {
+        int cellsize = java.lang.Float.SIZE/ Byte.SIZE;
+        ByteBuffer inputdata=ByteBuffer.allocateDirect(cellsize*3*windowsize).order(ByteOrder.nativeOrder());
+        if(!isFull())
+        {
+            AddMove(new MovePoint(0,0,0));
+            return ConvertDataToBuffer();
+        }
+        for(int i=0; i<windowsize;i++)
+        {
+            MovePoint nextMove=moves.get(i);
+            inputdata.putFloat(i* cellsize,nextMove.x);
+            inputdata.putFloat((i+windowsize)* cellsize,nextMove.y);
+            inputdata.putFloat((i+windowsize*2)* cellsize,nextMove.z);
+        }
+        return inputdata;
+    }
+    public boolean isFull()
+    {
+        return moves.size()==windowsize;
     }
 
 }
